50e3d2038bb84b787cc0eef7d652ff3ceed058c9
Syedazhar143 <156166271+Syedazhar143@users.noreply.github.com>
2024-01-28 14:28:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,7 +35,7 @@
 public class HelloWorld {
 public static void main(String[] args) {
     System.out.println("Hello, World");
-    System.out.println("Good Morning-->");
-    System.out.println("Good Morning-->"); 
+    System.out.println("Testing Webhook-->");
+    System.out.println("Testing Webhook-->"); 
   }
 }
